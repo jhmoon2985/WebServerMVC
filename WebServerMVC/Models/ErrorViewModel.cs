c7e057b9547cdb2bac25d01d039c558d0a8b6d40
tonyhong87 <131321637+tonyhong87@users.noreply.github.com>
2025-05-07 15:30:35 +0900

    초기 커밋.

---
+++ WebServerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebServerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
